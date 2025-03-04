218788a65dd696c04dcf775942e4ae36a7c1a593
0xdagarn <sgnoo@naver.com>
2025-03-04 12:18:29 +0900

    Add individual rule pages

--- lib/raffley/rules.ex
+++ lib/raffley/rules.ex
@@ -3,20 +3,28 @@ defmodule Raffley.Rules do
     [
       %{
         id: 1,
-        text: "Participate in the raffle by purchasing a ticket."
+        text: "Be kind and respectful to other participants"
       },
       %{
         id: 2,
-        text: "Each ticket costs $10."
+        text: "Keep your ticket safe - you'll need it to claim any prize"
       },
       %{
         id: 3,
-        text: "The raffle will be held on [DATE]."
+        text: "Make sure your contact details are up to date"
       },
       %{
         id: 4,
-        text: "The winner will be chosen at random from all the tickets sold."
+        text: "Have fun and enjoy the excitement of the raffle!"
       }
     ]
+  end
+
+  def get_rule(id) when is_integer(id) do
+    Enum.find(list_rules(), fn rule -> rule.id == id end)
+  end
+
+  def get_rule(id) when is_binary(id) do
+    id |> String.to_integer() |> get_rule()
   end
 end
