91849e2f42e800219f765f291fc2d1093d4c2d89
0xdagarn <sgnoo@naver.com>
2025-03-04 13:37:23 +0900

    Add raffle listing feature

--- lib/raffley_web/router.ex
+++ lib/raffley_web/router.ex
@@ -20,7 +20,9 @@ defmodule RaffleyWeb.Router do
     get "/", PageController, :home
     get "/rules", RuleController, :index
     get "/rules/:id", RuleController, :show
+
     live "/estimator", EstimatorLive
+    live "/raffles", RaffleLive.Index
   end
 
   # Other scopes may use custom stacks.
