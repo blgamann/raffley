bfea37bb348f418ae215b3671c2019b28dc4fc22
0xdagarn <sgnoo@naver.com>
2025-03-04 13:26:35 +0900

    Add auto-increment tickets feature

--- lib/raffley_web/controllers/live/estimator_live.ex
+++ lib/raffley_web/controllers/live/estimator_live.ex
@@ -2,10 +2,11 @@ defmodule RaffleyWeb.EstimatorLive do
   use RaffleyWeb, :live_view
 
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Process.send_after(self(), :tick, 2000)
+    end
+
     socket = socket |> assign(tickets: 0, price: 3)
-
-    IO.inspect(socket)
-
     {:ok, socket}
   end
 
@@ -44,5 +45,10 @@ defmodule RaffleyWeb.EstimatorLive do
   def handle_event("set-price", %{"price" => price}, socket) do
     socket = assign(socket, :price, String.to_integer(price))
     {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    Process.send_after(self(), :tick, 2000)
+    {:noreply, update(socket, :tickets, &(&1 + 10))}
   end
 end
