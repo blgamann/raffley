404b670e8ffcf712583b9a28c906cf62b45d4015
0xdagarn <sgnoo@naver.com>
2025-03-14 14:50:10 +0900

    feat: Add raffle editing functionality
    - Add get_raffle!/1 function to fetch specific raffle
    - Implement edit action in raffle form component
    - Add edit button and route in admin raffle list
    - Update router with edit path

--- lib/raffley_web/controllers/live/admin_raffle_live/form.ex
+++ lib/raffley_web/controllers/live/admin_raffle_live/form.ex
@@ -4,48 +4,62 @@ defmodule RaffleyWeb.AdminRaffleLive.Form do
   alias Raffley.Admin
   alias Raffley.Raffles.Raffle
 
-  def mount(_params, _session, socket) do
+  def mount(params, _session, socket) do
+    {:ok, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :new, _params) do
+    raffle = %Raffle{}
+
     changeset = Admin.change_raffle(%Raffle{})
 
-    socket =
-      socket
-      |> assign(:page_title, "New Raffle")
-      |> assign(:form, to_form(changeset))
+    socket
+    |> assign(:page_title, "New Raffle")
+    |> assign(:form, to_form(changeset))
+    |> assign(:raffle, raffle)
+  end
 
-    {:ok, socket}
+  defp apply_action(socket, :edit, %{"id" => id}) do
+    raffle = Admin.get_raffle!(id)
+
+    changeset = Admin.change_raffle(raffle)
+
+    socket
+    |> assign(:page_title, "Edit Raffle")
+    |> assign(:form, to_form(changeset))
+    |> assign(:raffle, raffle)
   end
 
   def render(assigns) do
     ~H"""
-    <div class="admin-raffle-form">
-      <.header>
-        {@page_title}
-      </.header>
-      <.simple_form for={@form} id="raffle-form" phx-submit="save" phx-change="validate">
-        <.input field={@form[:prize]} label="Prize" />
-        <.input field={@form[:description]} type="textarea" label="Description" phx-debounce="blur" />
-        <.input field={@form[:ticket_price]} type="number" label="Ticket Price" />
-        <.input
-          field={@form[:status]}
-          type="select"
-          label="Status"
-          prompt="Choose a status"
-          options={[:upcoming, :open, :closed]}
-        />
-        <.input field={@form[:image_path]} label="Image Path" />
-        <:actions>
-          <.button phx-disable-with="Saving...">Save Raffle</.button>
-        </:actions>
-      </.simple_form>
-      <.back navigate={~p"/admin/raffles"}>
-        Back
-      </.back>
-    </div>
+    {inspect(@live_action)}
+    <.header>
+      {@page_title}
+    </.header>
+    <.simple_form for={@form} id="raffle-form" phx-submit="save" phx-change="validate">
+      <.input field={@form[:prize]} label="Prize" />
+      <.input field={@form[:description]} type="textarea" label="Description" phx-debounce="blur" />
+      <.input field={@form[:ticket_price]} type="number" label="Ticket Price" />
+      <.input
+        field={@form[:status]}
+        type="select"
+        label="Status"
+        prompt="Choose a status"
+        options={[:upcoming, :open, :closed]}
+      />
+      <.input field={@form[:image_path]} label="Image Path" />
+      <:actions>
+        <.button phx-disable-with="Saving...">Save Raffle</.button>
+      </:actions>
+    </.simple_form>
+    <.back navigate={~p"/admin/raffles"}>
+      Back
+    </.back>
     """
   end
 
   def handle_event("validate", %{"raffle" => raffle_params}, socket) do
-    changeset = Admin.change_raffle(%Raffle{}, raffle_params)
+    changeset = Admin.change_raffle(socket.assigns.raffle, raffle_params)
     socket = assign(socket, :form, to_form(changeset, action: :validate))
     {:noreply, socket}
   end
